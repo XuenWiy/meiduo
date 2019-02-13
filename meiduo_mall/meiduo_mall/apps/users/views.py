c23f9645cebc4b5bef25128d67b87c2b568d7dc7
wuxin <1306539431@qq.com>
2019-02-13 20:01:53 +0800

    用户部分－用户名和手机号是否存在

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from users.models import User
+
+# 查询手机号是否已经注册
+# GET /mobiles/(?P<mobile>1[3-9]\d{9})/count/
+class MobileCountView(APIView):
+    def get(self, request, mobile):
+        """
+        获取用户名数量：
+         １．根据手机号查询数据库，获取查询结果数量
+         ２．返回手机号数量
+        """
+        # １．根据手机号查询数据库，获取查询结果数量
+        count = User.objects.filter(mobile=mobile).count()
+
+        # ２．返回手机号数量
+        res_data = {
+            'mobile':mobile,
+            'count':count
+        }
+        return Response(res_data)
+
+
+# 查询用户名是否存在
+# GET /usernames/(?P<username>\w{5,20})/count/
+class UsernameCountView(APIView):
+    def get(self, request, username):
+        """
+        获取用户名数量：
+         １．根据用户名查询数据库，获取查询结果数量
+         ２．返回用户名数量
+        """
+        # １．根据用户名查询数据库，获取查询结果数量
+        count = User.objects.filter(username=username).count()
+
+        # ２．返回用户名数量
+        res_data = {
+            'username':username,
+            'count':count
+        }
+        return Response(res_data)
