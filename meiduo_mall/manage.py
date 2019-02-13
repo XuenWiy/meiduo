9eee47702fcacf6b2ba393655bf5e29eddb2b262
wuxin <1306539431@qq.com>
2019-02-13 11:41:23 +0800

    框架搭建－子应用的创建和注册（搜索包目录列表添加）

--- meiduo_mall/manage.py
+++ meiduo_mall/manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # import sys
+    # print(sys.path)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_mall.settings.dev")
     try:
         from django.core.management import execute_from_command_line
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 将＇ａｐｐｓ＇添加到搜索包目录列表中
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+# print(sys.path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
