eedc1d46448dcfe5585f17e59fd80aa18a5cf215
wuxin <1306539431@qq.com>
2019-02-16 16:52:18 +0800

    地址管理-省市县三级联动(地区视图集)

--- meiduo_mall/meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -3,7 +3,13 @@ from areas import views
 
 
 urlpatterns = [
-    url(r'^areas/$', views.AreasView.as_view()),
-    url(r'^areas/(?P<pk>\d+)/$', views.SubAreasView.as_view()),
+    # url(r'^areas/$', views.AreasView.as_view()),
+    # url(r'^areas/(?P<pk>\d+)/$', views.SubAreasView.as_view()),
 
 ]
+
+# 路由Router
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('areas', views.AreasViewSet, base_name='areas')
+urlpatterns += router.urls
--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -3,11 +3,33 @@ from django.shortcuts import render
 from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from areas.models import Area
 from areas.serializers import AreaSerializer, SubAreaSerializer
 from rest_framework.mixins import RetrieveModelMixin
 
 # Create your views here.
+
+
+# 用视图集实现省市县地区查询
+class AreasViewSet(ReadOnlyModelViewSet):
+    """地区视图集"""
+    def get_serializer_class(self):
+        """获取视图所使用的序列化器类"""
+        if self.action == 'list':
+            return AreaSerializer
+        else:
+            return SubAreaSerializer
+
+    def get_queryset(self):
+        """获取视图所使用的查询集"""
+        if self.action == 'list':
+            return Area.objects.filter(parent=None)
+        else:
+            return Area.objects.all()
+
+
 
 
 # 获取市或县地区信息
