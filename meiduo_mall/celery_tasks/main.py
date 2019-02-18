e792c9694c4c04a468208e2e6c1cd034edef413a
wuxin <1306539431@qq.com>
2019-02-18 22:32:54 +0800

    商品部分-商品详情页静态化代码实现

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -12,4 +12,4 @@ celery_app = Celery('eityh')
 celery_app.config_from_object('celery_tasks.config')
 
 # 让celery worker启动是自动发现有那些任务函数
-celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email', 'celery_tasks.html'])
