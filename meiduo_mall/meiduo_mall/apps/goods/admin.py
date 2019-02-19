38c68ded634741cc593e4b6540a6c305b77d28df
wuxin <1306539431@qq.com>
2019-02-19 10:19:03 +0800

    商品部分-商品详情页静态页面更新(save_model和delete_model)

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -4,6 +4,49 @@ from goods import models
 
 # Register your models here.
 
+class SKUAdmin(admin.ModelAdmin):
+    """SKU模型Admin管理类"""
+    def save_model(self, request, obj, form, change):
+        # 数据保存
+        obj.save()
+
+        # 附加逻辑:发出重新生成静态详情页面的任务消息
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.id)
+
+
+
+class SKUSpecificationAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.sku.id)
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+
+class SKUImageAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.sku.id)
+
+        # 设置SKU默认图片
+        sku = obj.sku
+        if not sku.default_image_url:
+            sku.default_image_url = obj.image.url
+            sku.save()
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
 
 
 admin.site.register(models.GoodsCategory)
@@ -12,6 +55,6 @@ admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
 admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU)
-admin.site.register(models.SKUSpecification)
-admin.site.register(models.SKUImage)
+admin.site.register(models.SKU, SKUAdmin)
+admin.site.register(models.SKUSpecification, SKUSpecificationAdmin)
+admin.site.register(models.SKUImage, SKUImageAdmin)
